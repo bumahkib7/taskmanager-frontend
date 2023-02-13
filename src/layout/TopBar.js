e1739fe73ecd9d8838d92f0b8e1c740368120c0a
Bukhari bin mahmoud Kibuka <93518580+bumahkib7@users.noreply.github.com>
2023-02-13 18:05:29 +0100

    initial

---
+++ src/layout/TopBar.js
@@ -0,0 +1,33 @@
+import {AppBar, IconButton, Toolbar, Tooltip, Typography} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const TopBar = ({goHome, newTask, toggleDrawer}) => (
+  <AppBar position='fixed' sx={{zIndex: theme =>
+    theme.zIndex.drawer + 1}}>
+    <Toolbar>
+      <IconButton size='large' edge='start' color='inherit'
+        aria-label='menu'
+        onClick={toggleDrawer} >
+        <MenuIcon />
+      </IconButton>
+      <Tooltip title='Home'>
+        <IconButton color='inherit' onClick={goHome}>
+          <HomeOutlinedIcon />
+        </IconButton>
+      </Tooltip>
+      <Typography variant='h6' component='div' sx={{
+        flexGrow: 1 }}>
+        Task manager
+      </Typography>
+      <Tooltip title='Quick Add'>
+        <IconButton color='inherit' onClick={newTask}>
+          <AddIcon />
+        </IconButton>
+      </Tooltip>
+      <AccountCircleIcon />
+    </Toolbar>
+  </AppBar>
+);
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+//import from npm install @mui/icons-material
+import {createTheme} from '@mui/material/styles';
+import {red} from '@mui/material/colors';
+    
+export const theme = createTheme({
+    layout: {
+        drawerWidth: 240,
+    }
+});
+
+
